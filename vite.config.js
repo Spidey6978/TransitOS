98b3cdf9ef7faeffa808ecbf4b4fd8a627556ebc
Rhea <rheamisquitta@student.sfit.ac.in>
2026-04-17 17:39:17 +0530

    Initial commits

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // ADD THIS SECTION:
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://crank-chatting-broadcast.ngrok-free.dev',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            // This header bypasses the ngrok "interstitial" warning page
+            proxyReq.setHeader('ngrok-skip-browser-warning', 'true');
+          });
+        },
+      },
+    },
+  },
+})
